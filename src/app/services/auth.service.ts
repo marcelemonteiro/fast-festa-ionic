142df2e9487430828e17314a964e566ad4099fc3
marcelemonteiro <marcelepmonteiro@gmail.com>
2021-08-31 15:18:42 -0300

    Add services

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: AngularFireAuth) { }
+
+  login(user: User) {
+    return this.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
+
+  register(user: User) {
+    return this.auth.createUserWithEmailAndPassword(user.email, user.password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+}
