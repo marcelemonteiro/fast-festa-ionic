32235233e7af46dde9ac2fe61d6ec9562c0ed7a0
marcelemonteiro <marcelepmonteiro@gmail.com>
2021-09-10 00:16:45 -0300

    Add Register and Login Page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,78 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import {
+  LoadingController,
+  ModalController,
+  ToastController,
+} from '@ionic/angular';
+import { User } from '../interfaces/user';
 import { RegisterPage } from '../register/register.page';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss']
+  styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor(private modalController: ModalController) {}
+  userLogin: User = {};
+  constructor(
+    private modalController: ModalController,
+    private loadingController: LoadingController,
+    private toastController: ToastController,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
 
+  async login() {
+    await this.presentLoading();
+    try {
+      await this.authService.login(this.userLogin);
+      this.router.navigateByUrl('tabs/home');
+      this.dismissLoader();
+    } catch (error) {
+      this.presentToast(error.message);
+      console.log(error);
+      this.dismissLoader();
+    }
+  }
+
+  async presentRegister() {
+    const modal = await this.modalController.create({
+      component: RegisterPage,
+    });
+    await modal.present();
+  }
+
+  async presentLoading() {
+    const loader = await this.loadingController.create();
+    await loader.present();
+  }
+
+  async dismissLoader() {
+    let topLoader = await this.loadingController.getTop();
+    while (topLoader) {
+      if (!(await topLoader.dismiss())) {
+        break;
+      }
+      topLoader = await this.loadingController.getTop();
+    }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000,
+      color: 'danger',
+    });
+    return toast.present();
+  }
+
   async presentModalRegister() {
     const modal = await this.modalController.create({
-      component: RegisterPage
+      component: RegisterPage,
     });
     modal.present();
   }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import {
+  LoadingController,
+  ModalController,
+  ToastController,
+} from '@ionic/angular';
+import { User } from '../interfaces/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss']
+  styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  constructor(private modalController: ModalController) {}
+  userRegister: User = {};
+  private loading: any;
+
+  constructor(
+    private modalController: ModalController,
+    private loadingController: LoadingController,
+    private toastCtrl: ToastController,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
+
+  async register() {
+    await this.presentLoading();
+    try {
+      await this.authService.register(this.userRegister);
+      this.presentToast('Usuário cadastrado com sucesso', 'success');
+      this.dismissLoader();
+      this.modalController.dismiss();
+    } catch (error) {
+      this.presentToast(error.message, 'danger');
+      this.dismissLoader();
+    }
+  }
+
+  async presentLoading() {
+    const loader = await this.loadingController.create();
+    await loader.present();
+  }
+
+  async dismissLoader() {
+    let topLoader = await this.loadingController.getTop();
+    while (topLoader) {
+      if (!(await topLoader.dismiss())) {
+        break;
+      }
+      topLoader = await this.loadingController.getTop();
+    }
+  }
+
+  async presentToast(message: string, color: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000,
+      color,
+    });
+    return toast.present();
+  }
 
   dismissModal() {
     this.modalController.dismiss();
